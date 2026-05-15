1c6fe5532de02d6c17ed96ddd1ceffab120ca943
Yuvraj S Rana <yuvraj.rana@cloudstrats.com>
2026-05-15 14:15:30 +0530

    save

--- attendence/models.py
+++ attendence/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Attendance(models.Model):
+    employee = models.ForeignKey('employee.Employee', on_delete=models.CASCADE)
+    date = models.DateField()
+    check_in_time = models.TimeField()
+    check_out_time = models.TimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.employee.user.username} - {self.date}"
--- employee/models.py
+++ employee/models.py
@@ -7,8 +7,8 @@ class CustomUser(AbstractUser):
 
 class Employee(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    manager = models.ForeignKey('self', on_delete=models.SET_NULL)
+    department = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
     
-
     def __str__(self):
         return self.user.username
