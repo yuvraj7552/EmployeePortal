a89301feccfc4bdea63bd5009855b9f9fa56a9be
Yuvraj S Rana <yuvraj.rana@cloudstrats.com>
2026-05-15 10:50:42 +0530

    auth

--- employee/models.py
+++ employee/models.py
@@ -4,3 +4,11 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     pass
+
+class Employee(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    manager = models.ForeignKey('self', on_delete=models.SET_NULL)
+    
+
+    def __str__(self):
+        return self.user.username
